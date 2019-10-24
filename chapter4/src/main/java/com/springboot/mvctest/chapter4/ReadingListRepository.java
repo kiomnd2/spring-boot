ca69f258d4ed2506517642e11d507edb3d0baf75
김형익 <kiomnd2@naver.com>
2019-10-24 22:45:29 +0900

    REMOVE

--- chapter4/src/main/java/com/springboot/mvctest/chapter4/ReadingListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.mvctest.chapter4;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReadingListRepository extends JpaRepository<Book, Long> {
-    List<Book> findByReader(String reader);
-}
